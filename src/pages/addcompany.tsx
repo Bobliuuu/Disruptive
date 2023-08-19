f7afb487ee5180a7f9c256849b468d4369e84b43
isabellehuangg <huang.yuqing.issy@gmail.com>
2023-08-19 11:41:19 -0400

    Implemented Typeform to add companies to db

--- src/pages/addcompany.tsx
+++ src/pages/addcompany.tsx
@@ -1,1 +1,38 @@
+import Navbar from "~/components/navbar";
+import { selectionItems } from "~/constants/navbar";
+import React, { useEffect } from "react";
+
 // Typeform (add company page)
+export default function AddCompany() {
+  useEffect(() => {
+    // Load the Typeform script dynamically
+    const script = document.createElement("script");
+    script.src = "//embed.typeform.com/next/embed.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      // Clean up by removing the script when the component unmounts
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div className="dark-bg-gradient min-h-screeen">
+      <Navbar navbarData={selectionItems} />
+      
+      {/* Paste the Typeform embed code snippet here */}
+      <div
+        data-tf-widget="xEGXCFMK"
+        data-tf-opacity="100"
+        data-tf-iframe-props="title=Add Company"
+        data-tf-transitive-search-params
+        data-tf-medium="snippet"
+        className="w-11/12 md:w-10/12 lg:w-9/12 xl:w-8/12 mx-auto mt-8 pb-8"
+        style={{
+          height: "500px",
+        }}
+      ></div>
+    </div>
+  );
+}
